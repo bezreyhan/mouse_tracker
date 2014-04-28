db626049c94fcf2aa3a4da0748979e56af6ec96d
bezreyhan <bezreyhan@gmail.com>
2014-04-27 18:46:19 -0700

    add onload to copyscripts.js

--- public/javascripts/copyScripts.js
+++ public/javascripts/copyScripts.js
@@ -1,62 +1,63 @@
+window.onload = function() {
+  var trackOn = true;
+  var coordinatesString = "";
+  var timeOfLoad = new Date();
+  var windowLength = String(window.innerWidth -25)+"px";
+  var bodyHeight = String(document.body.scrollHeight)+"px";
+  var mouseTrackerUserId;
 
-var trackOn = true;
-var coordinatesString = "";
-var timeOfLoad = new Date();
-var windowLength = String(window.innerWidth -25)+"px";
-var bodyHeight = String(document.body.scrollHeight)+"px";
-var mouseTrackerUserId;
+  window.onmousemove = function (event) {
+    if (trackOn) {
+      printMovement(event);
+      trackOn = false;
+    }
+  };
 
-window.onmousemove = function (event) {
-  if (trackOn) {
-    printMovement(event);
-    trackOn = false;
+  function printMovement (event) {
+    coordinatesString += (event.pageX+","+event.pageY+";");
+    console.log("("+event.pageX+","+event.pageY+")");
+  }
+
+  mouseTrackTimer = window.setInterval(function() {
+    trackOn = true;
+  }, 10);
+
+
+  window.setInterval(
+    function sendData() {
+      console.log(coordinatesString);
+      $.ajax({
+        type: "POST",
+        url: 'http://mousemapper.herokuapp.com/interactions',
+        // datatType: 'jsonp',
+        data: {
+          interaction: {
+            move: coordinatesString,
+            user_id: mouseTrackerUserId,
+            time: timeOfLoad,
+            url: document.URL,
+            window_width: windowLength,
+            body_height: bodyHeight
+          }
+        },
+        success: function(response) {
+          alert('works');
+        }
+      });
+    }, 9000);
+
+  function displayMap() {
+    console.log("displayMap is fired");
+    var config = {
+      "radius": 50,
+      "element": document.getElementById("heatMapContainer"),
+      "visible": true,
+      "opacity": 100,
+      "gradient": { 0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)" }
+    };
+    var heatmap = heatmapFactory.create(config);
+    heatmap.store.setDataSet({ max: 10, data: cleanData } );
+    //change z-index inorder to make the heatMap the first layer
+    document.getElementById("heatMapContainer").style.zIndex="1";
   }
 };
-
-function printMovement (event) {
-  coordinatesString += (event.pageX+","+event.pageY+";");
-  console.log("("+event.pageX+","+event.pageY+")");
-}
-
-mouseTrackTimer = window.setInterval(function() {
-  trackOn = true;
-}, 10);
-
-
-window.setInterval(
-  function sendData() {
-    console.log(coordinatesString);
-    $.ajax({
-      type: "POST",
-      url: 'http://mousemapper.herokuapp.com/interactions',
-      // datatType: 'jsonp',
-      data: {
-        interaction: {
-          move: coordinatesString,
-          user_id: mouseTrackerUserId,
-          time: timeOfLoad,
-          url: document.URL,
-          window_width: windowLength,
-          body_height: bodyHeight
-        }
-      },
-      success: function(response) {
-        alert('works');
-      }
-    });
-  }, 9000);
-
-function displayMap() {
-  console.log("displayMap is fired");
-  var config = {
-    "radius": 50,
-    "element": document.getElementById("heatMapContainer"),
-    "visible": true,
-    "opacity": 100,
-    "gradient": { 0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)" }
-  };
-  var heatmap = heatmapFactory.create(config);
-  heatmap.store.setDataSet({ max: 10, data: cleanData } );
-  //change z-index inorder to make the heatMap the first layer
-  document.getElementById("heatMapContainer").style.zIndex="1";
-}
