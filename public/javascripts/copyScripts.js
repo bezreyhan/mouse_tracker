f9fbc9febb42c50ae43e2ad2e1a11f1c6827b26a
bezreyhan <bezreyhan@gmail.com>
2014-04-28 11:47:49 -0700

    debugging copyscripts

--- public/javascripts/copyScripts.js
+++ public/javascripts/copyScripts.js
@@ -41,25 +41,23 @@ window.onload = function() {
           }
         },
         success: function(response) {
-          alert('works');
-          console.log(mouseTrackerUserId);
-          console.log(smitty);
+          console.log('Data Sent');
         }
       });
     }, 9000);
 
-  function displayMap() {
-    console.log("displayMap is fired");
-    var config = {
-      "radius": 50,
-      "element": document.getElementById("heatMapContainer"),
-      "visible": true,
-      "opacity": 100,
-      "gradient": { 0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)" }
-    };
-    var heatmap = heatmapFactory.create(config);
-    heatmap.store.setDataSet({ max: 10, data: cleanData } );
-    //change z-index inorder to make the heatMap the first layer
-    document.getElementById("heatMapContainer").style.zIndex="1";
-  }
+  // function displayMap() {
+  //   console.log("displayMap is fired");
+  //   var config = {
+  //     "radius": 50,
+  //     "element": document.getElementById("heatMapContainer"),
+  //     "visible": true,
+  //     "opacity": 100,
+  //     "gradient": { 0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)" }
+  //   };
+  //   var heatmap = heatmapFactory.create(config);
+  //   heatmap.store.setDataSet({ max: 10, data: cleanData } );
+  //   //change z-index inorder to make the heatMap the first layer
+  //   document.getElementById("heatMapContainer").style.zIndex="1";
+  // }
 };
